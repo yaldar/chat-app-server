d804dfff13188f978e1fd5e17c6006d4ca31ed41
Yaldar <yaldar.hapak@gmail.com>
2020-09-26 09:39:36 +0200

    save point

--- index.js
+++ index.js
@@ -10,21 +10,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-var cookie = require('cookie');
+
 var PORT = 8080;
 var http = require('http').createServer(app);
+
 var io = require('socket.io')(http);
 
 const users = ['a'];
-
+// let users = [{ nickname: 'a' }];
+// NOTIFY WHEN NEW USER JOINS!!!!!!!!!!!!!
 // helpers:
+// const checkExists = (nickname) => users.find((el) => el.nickname === nickname);
 const checkExists = (nickname) => users.includes(nickname);
 
 app.get('/api/users', (req, res) => {
-  res.json(users).end();
+  res.json(users).status(200).end();
+  // res.json(users.map((el) => el.nickname)).end();
 });
 
 app.post('/api/users', (req, res) => {
+  console.log(users);
   const nickname = req.body.nickname;
   const alreadyExists = checkExists(nickname);
   if (alreadyExists) {
@@ -35,31 +40,27 @@ app.post('/api/users', (req, res) => {
   }
 });
 
-io.on('connection', function (socket) {
-  socket.on('test', (data) => {
-    console.log('on test ran ///');
+io.on('connect', function (socket) {
+  // socket.broadcast.emit('new_user', 'new user has joineed');
+  socket.on('message', (data) => {
+    socket.broadcast.emit(data);
+  });
 
-    console.log(data, socket.id, data);
-  });
-  socket.on('nickname', (data) => {
-    console.log('on nickname ran ///');
-    users.includes(data) ? null : users.push(data);
-    console.log(users);
-  });
+  // socket.on('disconnect', () => {
+
+  // });
 });
 
 // might delete later
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Page Not Found' });
-// });
+app.use((req, res) => {
+  res.status(404).json({ message: 'Page Not Found' });
+});
 
-// app.use((err, req, res, next) => {
-//   res
-//     .status(500)
-//     .json({
-//       message: `Something went wrong fetching the data. Try again later. Internal server error: "${err}"`,
-//     });
-// });
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: `Something went wrong fetching the data. Try again later. Internal server error: "${err}"`,
+  });
+});
 
 http.listen(PORT, function () {
   console.log('listening on port ' + PORT);
